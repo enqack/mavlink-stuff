84329f1146e65abfd132f14c62bba8f58995e233
Steve Verhelle <enqack@gmail.com>
2016-12-29 18:17:53 -0500

    fixed vi command mode clipping

--- mavlink_udp/mavlink_udp.c
+++ mavlink_udp/mavlink_udp.c
@@ -1,4 +1,5 @@
-opyright (C) 2010  Bryan Godbolt godbolt ( a t ) ualberta.ca
+/*******************************************************************************
+ Copyright (C) 2010  Bryan Godbolt godbolt ( a t ) ualberta.ca
  
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
